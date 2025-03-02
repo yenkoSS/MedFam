386fd6d4bfdf3e64fbeba81361398cfacbf2bcf6
Stefan Petru-Cornel <petrucornel.stefan@gmail.com>
2025-03-02 16:05:16 +0200

    add scroll reveal animations

--- js/script.js
+++ js/script.js
@@ -80,3 +80,33 @@ document.querySelectorAll(".nav-header-link-sm").forEach((link) => {
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   });
 });
+
+const textAboutList = document.querySelectorAll(".text-about");
+const cardsServicesList = document.querySelectorAll(".card-services");
+const cardsTeamList = document.querySelectorAll(".card-team");
+const cardsFaqList = document.querySelectorAll(".card-faq");
+const h3List = document.querySelectorAll("h3");
+const cardContact = document.querySelector(".card-contact");
+const imgAbout = document.querySelector(".box-img-about");
+
+const offsetYObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    entry.target.classList.toggle("offset-y0", entry.isIntersecting);
+    if (entry.isIntersecting) offsetYObserver.unobserve(entry.target);
+  });
+});
+
+const offsetXObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    entry.target.classList.toggle("offset-x0", entry.isIntersecting);
+    if (entry.isIntersecting) offsetXObserver.unobserve(entry.target);
+  });
+});
+
+textAboutList.forEach((text) => offsetYObserver.observe(text));
+cardsServicesList.forEach((card) => offsetYObserver.observe(card));
+cardsTeamList.forEach((card) => offsetYObserver.observe(card));
+cardsFaqList.forEach((card) => offsetYObserver.observe(card));
+h3List.forEach((h3) => offsetXObserver.observe(h3));
+offsetXObserver.observe(cardContact);
+offsetXObserver.observe(imgAbout);
